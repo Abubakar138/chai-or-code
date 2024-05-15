f4635acbb18e6644a212ace328294b10ccaa2b2a
Abubakar138 <78363664+Abubakar138@users.noreply.github.com>
2024-05-15 21:36:19 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 138
+let accountEmail = "abubakar@gmail.com"
+var accountPassword = "1122334455"
+accountCity = "Lahore"
+
+let accountState;
+
+// accountId = 1   // Not Allowed
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
